760fdc8c8cb672075167e59da070cb4436aa2ca8
Ian Foote <ian@feete.org>
2013-08-10 17:57:07 +0100

    Update 'collective' to 'garden'.

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ To create a log slider from 0.1 to 10 starting at 1 with major tick marks at
 every decade, and minor ticks at every 0.2 decades::
 
     from kivy.uix.slider import Slider
-    from kivy.collective.tickmarker import TickMarker
+    from kivy.garden.tickmarker import TickMarker
     class TickSlider(Slider, TickMarker):
         pass
     s = TickSlider(log=True, min_log=.1, max_log=10, value_log=1,
@@ -26,7 +26,7 @@ To create a log progress bar from 10 to 1000 starting at 500 with major tick
 marks at every decade, and minor ticks at every 0.1 decades::
 
     from kivy.uix.progressbar import ProgressBar
-    from kivy.collective.tickmarker import TickMarker
+    from kivy.garden.tickmarker import TickMarker
     class TickBar(ProgressBar, TickMarker):
         padding = NumericProperty(0)
         min = NumericProperty(0)
